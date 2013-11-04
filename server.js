9548dfd96cbb8f0318648e06f16e90264489e52a
BlairMBP <blairkelly1@gmail.com>
2013-11-03 22:27:16 -0500

    changed to express

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var thefile = config.xmlfile;
 var imagepath = config.imagepath;
 
 var fs = require('fs');
-var app = require('express')();
-var http = require('http');
 
+//var app = require('express')();
+//var http = require('http');
 
+var app = require('express')(),           // start Express framework
+    server = require('http').createServer(app); // start an HTTP server
 
+app.get('/', function (request, response) {
+  response.sendfile(__dirname + '/index.html');
+});
 
-http.createServer(function(req,resp) {
-    resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World " + thefile);
-    resp.end();
-    
-    console.log("sample output to console");
-}).listen(3002);
+server.listen(config.serverport);
