33ec56133e4d9ac259b66b7949542117e3be17ee
BlairMBP <blairkelly1@gmail.com>
2013-11-03 22:06:37 -0500

    added express, app

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var config = require('./config');
 var thefile = config.xmlfile;
 var imagepath = config.imagepath;
 
-var http = require('http');
+var fs = require('fs');
 
+var app = require('express')(),           // start Express framework
+    server = require('http').createServer(app); // start an HTTP server
+
+
+app.get('/', function (request, response) {
+  response.sendfile(__dirname + '/index.html');
+});
+
+server.listen(3002);
+
+/*
 http.createServer(function(req,resp) {
     resp.writeHead(200, {"Content-Type": "text/plain"});
     resp.write("Hello World " + thefile);
@@ -11,3 +22,4 @@ http.createServer(function(req,resp) {
     
     console.log("sample output to console");
 }).listen(3002);
+*/
