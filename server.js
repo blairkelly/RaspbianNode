4b5a0eec1e5f084a47291f05c1eb76be091da737
BlairMBP <blairkelly1@gmail.com>
2013-11-03 21:58:51 -0500

    spit out config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var config = require('./config');
+var thefile = config.xmlfile;
+var imagepath = config.imagepath;
 
 var http = require('http');
 
 http.createServer(function(req,resp) {
     resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World");
+    resp.write("Hello World " + thefile);
     resp.end();
     
     console.log("sample output to console");
-
 }).listen(3002);
